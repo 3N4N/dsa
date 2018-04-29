30e01d0d45c90a6a0d214bef726f42a5984a77a8
Enan Ajmain <3nan.ajmain@gmail.com>
2018-04-29 22:49:39 +0600

    Implemented Stack data structure

---
+++ Stack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+template <class T>
+class Stack
+{
+public:
+  Stack (int);
+  void push(T);
+  void pop();
+  T peek();
+  int size() { return top+1; }
+
+private:
+  T * arr;
+  int capacity;
+  int top;
+};
+
+template <class T>
+Stack<T>::Stack(int capacity) {
+  this->capacity = capacity;
+  top = -1;
+  arr = new T[capacity];
+}
+
+template <class T>
+void Stack<T>::push(T value) {
+  if (size() >= capacity) {
+    std::cout << "Stack is full." << std::endl;
+    return ;
+  }
+
+  top++;
+  arr[top] = value;
+}
+
+template <class T>
+void Stack<T>::pop() {
+  top--;
+}
+
+template <class T>
+T Stack<T>::peek() {
+  return arr[top];
+}
