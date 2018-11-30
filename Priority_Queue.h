4271d165733b442336f1924842bcb7673c6e8191
Enan Ajmain <3nan.ajmain@gmail.com>
2018-12-01 00:29:40 +0600

    Use MinHeap data structure for priority queue

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -3,110 +3,175 @@
 
 
 #include <iostream>
+#include "ArrayList.h"
 
-#define NULL_VALUE -99999
 
-template <class T>
-class Node
-{
-public:
-    Node (T value) {
-        this->value = value;
-        next = NULL;
-    }
-    virtual ~Node () {
-    }
-    void setNext(Node<T> * next) {
-        this->next = next;
-    }
-    void setValue(T value) {
-        this-> value = value;
-    }
-    void setPriority(int priority) {
-        this->priority = priority;
-    }
-    Node<T> * getNext() {
-        return next;
-    }
-    T getValue() {
-        return value;
-    }
-    int getPriority() {
-        return priority;
-    }
-
+template<class T>
+class MinHeap {
 private:
-    T value;
-    int priority;
-    Node<T> * next;
+    ArrayList<T> heap;
+    ArrayList<int> priority;
+    int left(int parent);                     // return index of left child
+    int right(int parent);                    // return index of right child
+    int parent(int child);                    // return index of parent
+    void increaseKey(int i, int v);
+    void decreaseKey(int i, int v);
+
+public:
+    MinHeap() { }
+    void insert(T element, int p);           // insert element into heap
+    void deleteMin();                        // delete minimum element
+    T extractMin();                          // extract minimum element
+    int size();                              // return heap size
+    void sift_down(int index);               // maintain heap structure
+    void sift_up(int index);                 // maintain heap structure
+    void buildHeap();
+    void updateKey(int i, int v);
+    void displayHeap();
 };
 
 template<class T>
-class Priority_Queue
-{
-private:
-    Node<T> *front;
-public:
-    Priority_Queue() {
-        front = NULL;
+int MinHeap<T>::size() {
+    return heap.size();
+}
+
+template<class T>
+int MinHeap<T>::left(int parent) {
+    int l = 2 * parent + 1;
+    return l;
+}
+
+template<class T>
+int MinHeap<T>::right(int parent) {
+    int r = 2 * parent + 2;
+    return r;
+}
+
+template<class T>
+int MinHeap<T>::parent(int child) {
+    int p = (child - 1)/2;
+    return p;
+}
+
+template<class T>
+void MinHeap<T>::insert(T element, int p) {
+    heap.add(element);
+    priority.add(p);
+}
+
+template<class T>
+void MinHeap<T>::deleteMin() {
+    heap.assign(0, heap.at(heap.size()-1));
+    priority.assign(0, priority.at(priority.size()-1));
+    heap.removeAt(heap.size() - 1);
+    priority.removeAt(priority.size() - 1);
+    sift_down(0);
+}
+
+template<class T>
+T MinHeap<T>::extractMin() {
+    return heap.at(0);
+}
+
+template<class T>
+void MinHeap<T>::increaseKey(int i, int v) {
+    priority.assign(i, v);
+    sift_down(i);
+}
+
+template<class T>
+void MinHeap<T>::decreaseKey(int i, int v) {
+    priority.assign(i, v);
+    sift_up(i);
+}
+
+template<class T>
+void MinHeap<T>::updateKey(int i, int v) {
+    if (v < priority.at(i))
+        decreaseKey(i, v);
+    else if (v > priority.at(i))
+        increaseKey(i, v);
+}
+
+template<class T>
+void MinHeap<T>::sift_down(int i) {
+    int n = heap.size();
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int least = i;
+    if (l < n && priority.at(l) < priority.at(least))
+        least = l;
+    if (r < n && priority.at(r) < priority.at(least))
+        least = r;
+    if (least != i) {
+        int tmp = priority.at(i);
+        priority.assign(i, priority.at(least));
+        priority.assign(least, tmp);
+        T tmpHeap = heap.at(i);
+        heap.assign(i, heap.at(least));
+        heap.assign(least, tmpHeap);
+        sift_down(least);
     }
-    void enqueue(T item, int priority);
-    T dequeue();
-    bool isEmpty();
-    bool isInQueue(T item);
+}
+
+template<class T>
+void MinHeap<T>::sift_up(int i) {
+    int n = heap.size();
+    int p = parent(i);
+    if( priority.at(i) < priority.at(p) ) {
+        int tmp = priority.at(i);
+        priority.assign(i, priority.at(p));
+        priority.assign(p, tmp);
+        T tmpHeap = heap.at(i);
+        heap.assign(i, heap.at(p));
+        heap.assign(p, tmpHeap);
+        sift_up(p);
+    }
+}
+
+template<class T>
+void MinHeap<T>::buildHeap() {
+    int n = heap.size();
+    for (int i=n/2-1; i>=0; i--) {
+        sift_down(i);
+    }
+}
+
+template<class T>
+void MinHeap<T>::displayHeap() {
+    heap.printArray();
+    std::cout << "\n";
+    priority.printArray();
+}
+
+
+/*
+ * Priority_Queue class starts here
+ */
+template<class T>
+class Priority_Queue {
+private:
+    MinHeap<T> heap;
+public:
+    Priority_Queue() { }
+    void insert(T item, int priority);
+    T extract_min();
+    int size() { return heap.size(); }
+    bool isEmpty() { return (size() == 0); }
+    void display() { heap.displayHeap(); }
 };
 
-template <class T>
-void Priority_Queue<T>::enqueue(T item, int priority) {
-    Node<T> *tmp;
-    Node<T> *q;
-    tmp = new Node<T>(item);
-    tmp->setPriority(priority);
-    if (front == NULL || priority < front->getPriority()) {
-        tmp->setNext(front);
-        front = tmp;
-    }
-    else {
-        q = front;
-        while (q->getNext() != NULL && q->getNext()->getPriority() <= priority)
-            q=q->getNext();
-        tmp->setNext(q->getNext());
-        q->setNext(tmp);
-    }
+template<class T>
+void Priority_Queue<T>::insert(T item, int priority) {
+    heap.insert(item, priority);
+    heap.sift_up(size() - 1);
 }
 
-template <class T>
-T Priority_Queue<T>::dequeue() {
-    Node<T> *tmp;
-    T ret;
-    if(front == NULL)
-        std::cout << "Queue Underflow\n";
-    else
-    {
-        tmp = front;
-        front = front->getNext();
-        ret = tmp->getValue();
-        free(tmp);
-    }
-    return ret;
-}
-
-template <class T>
-bool Priority_Queue<T>::isEmpty() {
-    if (front == NULL) return true;
-    else return false;
-}
-
-template <class T>
-bool Priority_Queue<T>::isInQueue(T item) {
-    if (front->getValue() == item)
-        return true;
-    Node<T> *tmp = front;
-    while (tmp->getNext()!=NULL) {
-        tmp = tmp->getNext();
-        if (tmp->getValue() == item) return true;
-    }
-    return false;
+template<class T>
+T Priority_Queue<T>::extract_min() {
+    T tmp = heap.extractMin();
+    heap.deleteMin();
+    return tmp;
 }
 
 #endif          // PRIORITY_QUEUE_H
